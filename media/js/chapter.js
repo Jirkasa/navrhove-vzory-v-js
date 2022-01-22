35600f8287fcfa6759bf180e67bcc696505ddae9
Jiří <satorajirka@seznam.cz>
2022-01-22 22:07:15 +0100

    make website responsive

--- media/js/chapter.js
+++ media/js/chapter.js
@@ -6,3 +6,13 @@ const header = document.getElementsByTagName("header")[0];
 makeStickyHeader(headerPlaceholder, header, "header--smaller");
 
 particlesJS.load('particles-js', '../media/assets/particlesjs-config-no-interaction.json');
+
+const navToggleButton = document.getElementById("navigation-toggle-button");
+const navCheckbox = document.getElementById("navigation-checkbox");
+navCheckbox.addEventListener("change", (e) => {
+    if (e.target.checked) {
+        navToggleButton.classList.add("nav-toggle-button--checked");
+    } else {
+        navToggleButton.classList.remove("nav-toggle-button--checked");
+    }
+});
